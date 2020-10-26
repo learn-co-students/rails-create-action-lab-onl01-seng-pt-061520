f8f6965e9945799e78f0bf2d84807988b6474ab2
kak79 <kohelka@gmail.com>
2020-10-26 10:12:49 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -17,9 +17,6 @@ class StudentsController < ApplicationController
     @student.first_name = params[:first_name]
     @student.last_name = params[:last_name]
     @student.save
-    redirect_to student_path(@student)
-    
-  end
-
-  
+    redirect_to student_path(@student)  
+  end 
 end
